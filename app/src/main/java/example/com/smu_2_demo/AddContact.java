78a4d61df5717b246eb7c31d899fdba6874e2e8e
lee-soyong12 <dlthfyd708@naver.com>
2017-07-20 16:26:24 +0900

    이소용 : base

---
+++ app/src/main/java/example/com/smu_2_demo/AddContact.java
@@ -0,0 +1,48 @@
+package example.com.smu_2_demo;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AddContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+
+        final TextView nameinput = (TextView) findViewById(R.id.nameet);
+        final TextView codeinput = (TextView) findViewById(R.id.CODETEXT3);
+        Button saveButton = (Button) findViewById(R.id.button);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameinput.getText().toString();
+                String code = codeinput.getText().toString();
+
+                SharedPreferences pref = getSharedPreferences("MAIN",MODE_PRIVATE);
+                pref.edit().putString(name, code).apply();
+                finish();
+            }
+
+        });
+    }
+}
+
+ /*nameInput.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View view){
+                Log.d("my","nameinput");
+            }
+        });
+
+        codeInput.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d("my","codeinput");
+            }
+        });*/
---
+++ app/src/main/java/example/com/smu_2_demo/DetailContact.java
@@ -0,0 +1,25 @@
+package example.com.smu_2_demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_contact);
+        TextView nameText = (TextView) findViewById(R.id.nametext);
+        TextView codeText = (TextView) findViewById(R.id.codetext);
+
+        Intent receiveintent = getIntent();
+        Bundle extras = receiveintent.getExtras();
+        String name = extras.getString("name","UNKNOWN");
+        String code = extras.getString("code", "UNKNOWN");
+
+        nameText.setText(name);
+        codeText.setText(code);
+    }
+}
